fae12ace3373de9d07e54b631936d9ca49a7b424
Y <y@example.com>
2020-11-10 20:00:19 +0800

    增加多端口绑定支持

--- config/app.php
+++ config/app.php
@@ -19,8 +19,8 @@ return [
     'app_map'          => [],
     // 域名绑定（自动多应用模式有效）
     'domain_bind' => [
-        'admin' => 'admin',
-        'www' => 'index'
+//        'admin' => 'admin',
+//        'www' => 'index'
     ],
     // 禁止URL访问的应用列表（自动多应用模式有效）
     'deny_app_list'    => [],
